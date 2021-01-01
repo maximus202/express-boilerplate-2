27439afbc31ea3dfcc2ddd52ac1e8a3af7f4d0dd
Max R <maxx.ratto@gmail.com>
2021-01-01 09:09:26 -0700

    fixed semi colons

--- src/app.js
+++ src/app.js
@@ -1,28 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
 
-const app = express()
+const app = express();
 
-app.use(helmet())
-app.use(cors())
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
-})
+});
 
+//conditionally returns an error message based on the environment
 app.use(function errorHandler(error, req, res, next) {
     let response
+
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
+        response = {
+            error:
+            {
+                message: 'server error'
+            }
+        };
     }
-    res.status(500).json(response)
-})
+    else {
+        console.error(error)
+        response = {
+            message: error.message, error
+        }
+    }
+
+    res.status(500).json(response);
+});
 
 
-module.exports = app
+module.exports = app;
