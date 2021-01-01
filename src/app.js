1f7eb2ff6651fca21989c42fa7b8bed81c0bc291
Max R <maxx.ratto@gmail.com>
2021-01-01 08:58:31 -0700

    uninstalled morgan and removed usage in app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
-
-app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
